e18cab03e203672168ee28827734cc199cc6a0f2
Douglas Hall <douglas_hall@harvard.edu>
2015-09-09 13:38:27 +0000

    Initial build out of the project and account dashboard

---
+++ canvas_admin_tools/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+
+from canvas_admin_tools import views
+
+
+urlpatterns = [
+    url(r'^lti_auth_error/', views.lti_auth_error, name='lti_auth_error'),
+    url(r'^tool_config_account$', views.tool_config_account, name='tool_config_account'),
+    url(r'^tool_config_course$', views.tool_config_account, name='tool_config_course'),
+    url(r'^lti_launch$', views.lti_launch, name='lti_launch'),
+    url(r'^account_dashboard$', views.dashboard_account, name='dashboard_account'),
+    url(r'^course_dashboard$', views.dashboard_course, name='dashboard_course'),
+]
