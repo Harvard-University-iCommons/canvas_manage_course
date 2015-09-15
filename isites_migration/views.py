519189e304e16a1331be17b36efa965df32ddaaf
Douglas Hall <douglas_hall@harvard.edu>
2015-09-15 14:23:06 +0000

    Display select options of iSites keywords to import

--- isites_migration/views.py
+++ isites_migration/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 
 from async.models import Process
 
+from icommons_common.models import CourseInstance, SiteMap
+
 from isites_migration.jobs import migrate_files
 
 
@@ -13,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 @login_required
 def index(request):
+    course_instance_id = request.LTI.get('lis_course_offering_sourcedid')
     canvas_course_id = request.LTI.get('custom_canvas_course_id')
     if request.method == 'POST':
         keyword = request.POST.get('keyword')
@@ -23,7 +26,18 @@ def index(request):
             queue='isites_file_migration'
         )
 
+    try:
+        course_instance = CourseInstance.objects.get(course_instance_id=course_instance_id)
+        course_instance_ids = [c.course_instance_id for c in course_instance.course.course_instances.all()]
+        keywords = [m.course_site.external_id for m in SiteMap.objects.filter(
+            course_instance_id__in=course_instance_ids,
+            course_site__site_type_id='isite'
+        )]
+    except CourseInstance.DoesNotExist:
+        keywords = []
+
     processes = Process.objects.filter(name='isites_migration.jobs.migrate_files').order_by('-date_created')
     return render(request, 'isites_migration/index.html', {
+        'keywords': keywords,
         'processes': processes
     })
