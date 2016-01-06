56f753800ad9bef70be55a352a23eb9d2c5a12bb
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2016-01-06 17:08:18 -0500

    Migrate settings for the isites_migration app that are performed on tool2 to the sites_migration settings file.  Introduce a settings file for the sites export tool that will be paired with a management command call to export a given iSite.  Use aws profiles in place of secure settings for key and secret - the access key and ids for each profile are managed in the .aws/credentials file on tool2.  Use boto3 for file upload and url generation in place of boto 2 for speed and for built-in support for > 5GB file uploads.

---
+++ canvas_course_admin_tools/settings/isites_export_tool.py
@@ -0,0 +1,19 @@
+from .aws import *
+
+INSTALLED_APPS = (
+    'icommons_common',
+    'async',
+    'django_rq',
+    'isites_migration',
+)
+
+# Settings specifically for the isite_migration LTI app
+AWS_EXPORT_BUCKET_ISITES_FILES = 'isites-exports-prod'
+AWS_EXPORT_BUCKET_SLIDE_TOOL = 'isites-slide-data'
+AWS_EXPORT_DOWNLOAD_TIMEOUT_SECONDS = 60
+AWS_PROFILE = 'isites_export_tool'
+
+EXPORT_DIR = os.path.join(BASE_DIR, 'isites_export_tool')  # local directory where export files are stored
+EXPORT_ARCHIVE_FILENAME_PREFIX = ''
+EXPORT_FILES_EXCLUDED_TOOL_IDS = []
+EXPORT_FILES_EXCLUDED_TOPIC_TITLES = []
