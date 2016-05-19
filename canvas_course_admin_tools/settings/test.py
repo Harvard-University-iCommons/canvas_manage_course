44458b05d87907a0c3151efd325b45e17eb26544
Yates, Elliott <elliott_yates@harvard.edu>
2016-05-19 16:35:18 -0400

    tlt-1870: manage people - enable skipped unit tests; unit tests now run under sqlite

--- canvas_course_admin_tools/settings/test.py
+++ canvas_course_admin_tools/settings/test.py
@@ -1,17 +1,20 @@
 from .local import *
 
-# Make tests faster
+DATABASE_APPS_MAPPING = {
+    'icommons_common': 'default',
+    'isites_migration': 'default',
+    'lti_permissions': 'default',
+    'manage_people': 'default',
+    'manage_sections': 'default',
+}
 
-DATABASE_ROUTERS = []
+DATABASE_MIGRATION_WHITELIST = ['default']
 
+# Make tests faster by using sqlite3
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': SECURE_SETTINGS.get('db_default_name', 'canvas_admin_tools'),
-        'USER': SECURE_SETTINGS.get('db_default_user', 'postgres'),
-        'PASSWORD': SECURE_SETTINGS.get('db_default_password'),
-        'HOST': SECURE_SETTINGS.get('db_default_host', '127.0.0.1'),
-        'PORT': SECURE_SETTINGS.get('db_default_port', 5432),  # Default postgres port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
     }
 }
 
@@ -23,3 +26,14 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache'
     }
 }
+
+"""
+Unit tests should be able to run without talking to external services like
+redis/elasticache.  Let's disable the caching middleware, and use the db
+to store session data.
+"""
+MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
+MIDDLEWARE_CLASSES[MIDDLEWARE_CLASSES.index('cached_auth.Middleware')] = \
+    'django.contrib.auth.middleware.AuthenticationMiddleware'
+MIDDLEWARE_CLASSES = tuple(MIDDLEWARE_CLASSES)
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
