47810ac1c6c59c27dc95636209a816f259a65567
hktest <hiukei_chow@harvard.edu>
2016-09-01 18:40:37 -0400

    - Updated and added to existing manage people permissions tests - Added all the various roles that will be under test for the test course and cleaned up for a cleaner looking format.

--- selenium_tests/manage_people/mp_permission_tests.py
+++ selenium_tests/manage_people/mp_permission_tests.py
@@ -1,17 +1,21 @@
+from datetime import datetime
+
 from ddt import ddt, data, unpack
 
 from selenium_common.base_test_case import get_xl_data
 from selenium_tests.manage_people.base_test_case import \
     MP_PERMISSION_ROLES
+from selenium_tests.manage_people.page_objects.mp_main_page import MainPageObject
 from selenium_tests.course_admin.course_admin_base_test_case import \
     CourseAdminBaseTestCase
+
 
 
 @ddt
 class MPPermissionTests(CourseAdminBaseTestCase):
     @data(*get_xl_data(MP_PERMISSION_ROLES))
     @unpack
-    def test_roles_access(self, user_id, given_access):
+    def test_roles_access(self, user_id, given_access, expected_role):
         # This test masquerades as users in roles in the spreadsheet
         # specified in MP_PERMISSION_ROLES, and then validates the the users
         # are granted/denied access based on their role.
@@ -20,24 +24,33 @@ class MPPermissionTests(CourseAdminBaseTestCase):
         # Go back to the Manage Course Dashboard
         self.driver.get(self.TOOL_URL)
 
-
         if given_access == 'yes':
             self.assertTrue(
                 self.course_admin_dashboard_page
                     .manage_people_button_is_displayed(),
-                'User {} should have access to see tool but does not'.format(
-                    user_id)
+                'User {} with expected role {} should see the manage people '
+                'button on page but does not.'.format(user_id, expected_role)
             )
+
+            #  Clicks into tool
+            self.course_admin_dashboard_page.select_manage_people_link()
+            # Verifies that user can see the manage people tool
+
+            manage_people_page = MainPageObject(self.driver)
+            # Switch frame and verify main page is loaded
+            manage_people_page.focus_on_tool_frame()
+            self.assertTrue(manage_people_page.is_loaded())
 
         elif given_access == 'no':
             self.assertFalse(
                 self.course_admin_dashboard_page
-                    .manage_people_button_is_displayed(),
-                'User {} does not see the tool, but should.'.format(user_id)
+                    .manage_people_button_is_displayed(), 'User {} with '
+                'expected role {} should not see the manage_sections button, '
+                'but can see it.'.format(user_id, expected_role)
             )
 
         else:
             raise ValueError(
-                'given_access column for user {} must be either \'yes\' or '
-                '\'no\''.format(user_id)
+                'given_access column for user {} with expected role {} must be '
+                'either "yes" or "no".'.format(user_id, expected_role)
             )
