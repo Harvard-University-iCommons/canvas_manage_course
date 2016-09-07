705b2e29546d2d8733f99d01edde6be792aa0cd3
hktest <hiukei_chow@harvard.edu>
2016-09-07 15:22:42 -0400

    - removing unused coded. - cleaned up db records and added test data per the ACP requirements in TLT-2650. - updated permissions test to account for the scenario in which some users may have even get past the tool permission checks. because there is more restrictive permissioning in place at the canvas_manage_course level. - the tests passed.

--- selenium_tests/manage_sections/manage_sections_base_test_case.py
+++ selenium_tests/manage_sections/manage_sections_base_test_case.py
@@ -1,17 +1,12 @@
-from django.conf import settings
 from os.path import abspath, dirname, join
 
 from selenium_tests.course_admin.course_admin_base_test_case \
     import CourseAdminBaseTestCase
-from selenium_tests.course_admin.page_objects\
-    .course_admin_dashboard_page_object import CourseAdminDashboardPage
-from selenium_tests.manage_sections.page_objects.manage_sections_main_page \
-    import MainPageObject
 
 
 # Common files used for all Manage Sections test case:
 MANAGE_SECTION_PERMISSIONS = join(dirname(abspath(__file__)), 'test_data',
-                                  'permissions_roles_access.xlsx')
+                                  'manage_sections_roles_access.xlsx')
 
 
 class ManageSectionsBaseTestCase(CourseAdminBaseTestCase):
