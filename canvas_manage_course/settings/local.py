8af5a85bba5e54bbdfa208caf70b1cfb0dcaf032
Yates, Elliott <elliott_yates@harvard.edu>
2017-02-23 09:21:55 -0500

    tlt-2990: cleans up some unused local settings

--- canvas_manage_course/settings/local.py
+++ canvas_manage_course/settings/local.py
@@ -10,9 +10,6 @@ MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
 # For Django Debug Toolbar:
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
 
 # Log to console instead of a file when running locally
 LOGGING['handlers']['default'] = {
