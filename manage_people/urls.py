b83b0edb1dc5c7c4b031ac153756881fd2928da6
Yates, Elliott <elliott_yates@harvard.edu>
2017-02-23 08:54:16 -0500

    tlt-2967: upgrades Django from 1.9 to 1.10

--- manage_people/urls.py
+++ manage_people/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
+from manage_people import views
 
-urlpatterns = patterns('',
-    url(r'^user_form$', 'manage_people.views.user_form', name='user_form'),
-    url(r'^results_list$', 'manage_people.views.results_list', name='results_list'),
-    url(r'^add_users$', 'manage_people.views.add_users', name='add_users'),
-    url(r'^remove_user$', 'manage_people.views.remove_user', name='remove_user'),
-    url(r'^find_user$', 'manage_people.views.find_user', name='find_user'),
-)
+urlpatterns = [
+    url(r'^user_form$', views.user_form, name='user_form'),
+    url(r'^results_list$', views.results_list, name='results_list'),
+    url(r'^add_users$', views.add_users, name='add_users'),
+    url(r'^remove_user$', views.remove_user, name='remove_user'),
+    url(r'^find_user$', views.find_user, name='find_user'),
+]
