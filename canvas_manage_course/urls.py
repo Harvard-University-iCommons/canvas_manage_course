7d010d1ddfbde55f5d01b32714a4e725b736c209
Sapna Mysore <sapna_mysore@harvard.edu>
2019-10-28 16:54:19 -0400

    more url fixes

--- canvas_manage_course/urls.py
+++ canvas_manage_course/urls.py
@@ -9,13 +9,14 @@ urlpatterns = [
     path('course_dashboard', views.dashboard_course, name='dashboard_course'),
     path('class_roster/', include(('class_roster.urls','class_roster'), namespace='class_roster')),
     path('isites_migration/', include(('isites_migration.urls','isites_migration'), namespace='isites_migration')),
-    path('lti_auth_error$', icommons_ui_views.not_authorized, name='lti_auth_error'),
-    path('lti_launch$', views.lti_launch, name='lti_launch'),
+    path('lti_auth_error', icommons_ui_views.not_authorized, name='lti_auth_error'),
+    path('lti_launch', views.lti_launch, name='lti_launch'),
+
     path('manage_people/', include(('manage_people.urls','manage_people'), namespace='manage_people')),
     path('manage_school_permissions/', include(('lti_school_permissions.urls','manage_school_permissions'),
                                                namespace='manage_school_permissions')),
     path('manage_sections/', include(('manage_sections.urls','manage_sections'), namespace='manage_sections')),
-    path('not_authorized$', icommons_ui_views.not_authorized, name='not_authorized'),
+    path('not_authorized', icommons_ui_views.not_authorized, name='not_authorized'),
     path('tool_config', views.tool_config, name='tool_config'),
 ]
 
