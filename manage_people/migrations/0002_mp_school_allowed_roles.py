8e99fbfb7e8e9959bc72cbf2fbbbf11ef6c1fb7f
Sapna Mysore <sapna_mysore@harvard.edu>
2019-11-05 13:05:54 -0500

    Fixed the migartions

--- manage_people/migrations/0002_mp_school_allowed_roles.py
+++ manage_people/migrations/0002_mp_school_allowed_roles.py
@@ -2,6 +2,8 @@
 
 
 from django.db import  migrations, models,transaction
+import django.db.models.deletion
+
 
 SCHOOL_ALOWED_ROLE_DATA = [
     ('gse', 9, False),
@@ -92,7 +94,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('school_id', models.CharField(max_length=10)),
-                ('user_role', models.ForeignKey(to='manage_people.ManagePeopleRole')),
+                ('user_role', models.ForeignKey(to='manage_people.ManagePeopleRole', on_delete=django.db.models.deletion.CASCADE)),
                 ('xid_allowed', models.BooleanField(default=False)),
             ],
             options={
