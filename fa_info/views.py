fe63a60b22ea689f5e06cad8e7d2557cdb6e2846
dodget <tylor_dodge@harvard.edu>
2020-01-16 08:31:30 -0500

    Add fa_info app to course manager.

---
+++ fa_info/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
+from icommons_common.models import CourseInstance
+from lti_school_permissions.decorators import lti_permission_required
+
+@login_required
+@lti_permission_required('fa_info')
+@require_http_methods(['GET'])
+def index(request):
+    """ Returns a redirect to the course's FAINFO page """
+    course_instance_id = request.LTI['lis_course_offering_sourcedid']
+    course_instance = CourseInstance.objects.get(course_instance_id=course_instance_id)
+    
+    term_id = course_instance.term.cs_strm
+    course_id = course_instance.course.registrar_code
+    url = "https://hrvc9dev.my.harvard.edu/psc/hrvc9dev/EMPLOYEE/HRMS/c/HU_FIN"\
+        "AL_ASSMNT.HU_FINAL_ASSMNT.GBL?Page=HU_EXAM_ROSTER_INS&Action=U&ExactK"\
+        "eys=Y&INSTITUTION=HRVRD&ACAD_CAREER=FAS&STRM={}&CRSE_ID={}".format(term_id, course_id)
+    if not term_id or not course_id:
+        return render(request, 'fa_info/index.html')
+    return redirect(url)
