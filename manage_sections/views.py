3ff7ba025a701820bcab9778ef9fda067fbe7fc6
Patrick Tonne <patrick_tonne@harvard.edu>
2021-05-14 17:46:37 -0400

    [TLT-2020] update remove_section view logic to handle new utility function

--- manage_sections/views.py
+++ manage_sections/views.py
@@ -28,6 +28,7 @@ from .utils import (
     is_enrollment_section,
     is_sis_section,
     is_credit_status_section,
+    delete_enrollments
 )
 
 
@@ -333,20 +334,17 @@ def remove_section(request, section_id):
     if len(enrollments) == 0:
         section = canvas_api_helper_sections.delete_section(canvas_course_id, section_id)
         return JsonResponse(section)
-    else:
-        # delete enrollments, then delete section
-        for enrollment in enrollments:
-            user_section_id = enrollment.get('id', '')
-            if not user_section_id:
-                return JsonResponse({'message': "Invalid user_section_id %s" % user_section_id}, status=500)
-            
-            canvas_api_helper_enrollments.conclude_enrollment(
-                SDK_CONTEXT, canvas_course_id, user_section_id
-            )
-            logger.debug(f'Successful deletion of user {user_section_id} from course {canvas_course_id}')
-        section = canvas_api_helper_sections.delete_section(canvas_course_id, section_id)
+    
+    # delete enrollments, then delete section
+    responses, is_empty = delete_enrollments(enrollments, canvas_course_id)
+    if not is_empty:
+        return JsonResponse({
+            'message': f'Issue clearing enrollments prior to deletion. Unable to delete section {section_id} from course {canvas_course_id}'},
+            status=500
+        )
+    section = canvas_api_helper_sections.delete_section(canvas_course_id, section_id)
 
-        return JsonResponse(section)
+    return JsonResponse(section)
 
 
 @login_required
